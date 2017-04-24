6ac97f01d05618d28ac78ae1c723d46f952fe583
Ivan5646 <Mike000027@yandex.ru>
2017-04-24 23:53:53 +0700

    user alreade exists

--- php_login/register.php
+++ php_login/register.php
@@ -1,5 +1,7 @@
 <?php
 
+//error_reporting(E_ERROR); // remove
+
 session_start();
 
 include 'dbconnect.php';
@@ -8,10 +10,25 @@ $name = $_POST['uname'];
 $email = $_POST['email'];
 $pass = $_POST['password'];
 
-$encrypted_pass = hash('sha256', $pass);
+// $encrypted_pass = hash('sha256', $pass);
+// $sql = "INSERT INTO users (userName, userEmail, userPass) VALUES ('$name', '$email', '$encrypted_pass')";
+// $result = $conn->query($sql);
+// header("Location: ../index.php") // redirects to index.php
 
-$sql = "INSERT INTO users (userName, userEmail, userPass) VALUES ('$name', '$email', '$encrypted_pass')";
+
+$encrypted_pass =  hash('sha256', $pass); 
+
+// check if user already exists
+$sql = "SELECT * FROM users WHERE userEmail='$email'";
 $result = $conn->query($sql);
+if(mysqli_num_rows($result)==TRUE){
+  echo"name already exists";
+}else{
+  //$encrypted_pass = hash('sha256', $pass);
+  $sql = "INSERT INTO users (userName, userEmail, userPass) VALUES ('$name', '$email', '$encrypted_pass')";
+  $result = $conn->query($sql);
+  header("Location: ../index.php"); // redirects to index.php
+}
 
-header("Location: ../index.php") // redirects to index.php
+
 ?>
---
+++ trash/draft.php
@@ -0,0 +1,79 @@
+C:\OpenServer\domains\uRegMine\php_login\register.php on line 24
+
+Warning: Cannot modify header information - headers already sent by (output started at C:\OpenServer\domains\uRegMine\php_login\register.php:24) in C:\OpenServer\domains\uRegMine\php_login\register.php on line 30
+
+solved by siingle quotation marks around $email
+$sql = "SELECT * FROM users WHERE userEmail='$email'";
+
+//register.php
+<?php
+
+error_reporting(E_ERROR); // remove
+
+session_start();
+
+include 'dbconnect.php';
+
+$name = $_POST['uname'];
+$email = $_POST['email'];
+$pass = $_POST['password'];
+
+// $encrypted_pass = hash('sha256', $pass);
+// $sql = "INSERT INTO users (userName, userEmail, userPass) VALUES ('$name', '$email', '$encrypted_pass')";
+// $result = $conn->query($sql);
+// header("Location: ../index.php") // redirects to index.php
+
+
+$encrypted_pass =  hash('sha256', $pass); 
+
+// check if user already exists
+$sql = "SELECT * FROM users (userName, userEmail) WHERE userEmail=$email";
+$result = $conn->query($sql);
+if(mysql_num_rows($result)>=1){
+  echo"name already exists";
+}else{
+  //$encrypted_pass = hash('sha256', $pass);
+  $sql = "INSERT INTO users (userName, userEmail, userPass) VALUES ('$name', '$email', '$encrypted_pass')";
+  $result = $conn->query($sql);
+  header("Location: ../index.php"); // redirects to index.php
+}
+
+
+?>
+
+
+// worked
+<?php
+
+//error_reporting(E_ERROR); // remove
+
+session_start();
+
+include 'dbconnect.php';
+
+$name = $_POST['uname'];
+$email = $_POST['email'];
+$pass = $_POST['password'];
+
+// $encrypted_pass = hash('sha256', $pass);
+// $sql = "INSERT INTO users (userName, userEmail, userPass) VALUES ('$name', '$email', '$encrypted_pass')";
+// $result = $conn->query($sql);
+// header("Location: ../index.php") // redirects to index.php
+
+
+$encrypted_pass =  hash('sha256', $pass); 
+
+// check if user already exists
+$sql = "SELECT * FROM users WHERE userEmail='asya@ya.ru'";
+$result = $conn->query($sql);
+if(mysqli_num_rows($result)==TRUE){
+  echo"name already exists";
+}else{
+  //$encrypted_pass = hash('sha256', $pass);
+  $sql = "INSERT INTO users (userName, userEmail, userPass) VALUES ('$name', '$email', '$encrypted_pass')";
+  $result = $conn->query($sql);
+  header("Location: ../index.php"); // redirects to index.php
+}
+
+
+?>
